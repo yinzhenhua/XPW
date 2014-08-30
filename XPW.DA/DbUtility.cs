5fb7075b7c1eb6405c1b14ccaf8dd5447097b288
yinzhen_hua@163.com <Pass@word1>
2014-08-30 19:24:32 +0800

    https://github.com/yinzhenhua/XPW.git

---
+++ XPW.DA/DbUtility.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data;
+
+namespace XPW.DA
+{
+    public class DbUtility
+    {
+        private DbUtility()
+        {
+
+        }
+
+        Database _database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+        private static object _asyncObject = new object();
+        private static DbUtility _instance;
+        
+        public static DbUtility Instance
+        {
+            get
+            {
+                lock(_asyncObject)
+                {
+                    if(null==_instance)
+                    {
+                        _instance = new DbUtility();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void FillDataSet(string procName, DataSet ds, string tableName)
+        {
+            using (var command = _database.GetStoredProcCommand(procName))
+            {
+                _database.LoadDataSet(command, ds, tableName);
+            }
+        }
+
+        public void FillDataSet(string procName, DataSet ds, string tableName, object[] parameters)
+        {
+            using (var command = _database.GetStoredProcCommand(procName, parameters))
+            {
+                _database.LoadDataSet(command, ds, tableName);
+            }
+        }
+
+        public object LoadScalar(string procName)
+        {
+            using (var command = _database.GetStoredProcCommand(procName))
+            {
+                return _database.ExecuteScalar(command);
+            }
+        }
+    }
+}
---
+++ XPW/Contact.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="XPW.Contact" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <asp:Chart ID="Chart1" runat="server">
+        <Series>
+            <asp:Series Name="Series1"></asp:Series>
+        </Series>
+        <ChartAreas>
+            <asp:ChartArea Name="ChartArea1"></asp:ChartArea>
+        </ChartAreas>
+    </asp:Chart>
+</asp:Content>
